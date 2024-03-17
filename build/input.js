746fd54a9299f001e315c1963ec825900e14677b
Paul Miller <paul@paulmillr.com>
2024-03-17 13:26:11 +0000

    Use jsbt for tsconfig and building

---
+++ build/input.js
@@ -0,0 +1,11 @@
+export { ml_kem512, ml_kem768, ml_kem1024 } from '@noble/post-quantum/ml-kem';
+export { kyber512, kyber768, kyber1024 } from '@noble/post-quantum/ml-kem';
+export { ml_dsa44, ml_dsa65, ml_dsa87 } from '@noble/post-quantum/ml-dsa';
+export {
+  slh_dsa_sha2_128f,
+  slh_dsa_sha2_128s,
+  slh_dsa_sha2_192f,
+  slh_dsa_sha2_192s,
+  slh_dsa_sha2_256f,
+  slh_dsa_sha2_256s,
+} from '@noble/post-quantum/slh-dsa';
