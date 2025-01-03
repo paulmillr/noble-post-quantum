019eb9b64449eaaa08d1edc92be61c112c642d4e
Paul Miller <paul@paulmillr.com>
2025-01-03 01:53:01 +0000

    Add top-level docs

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,19 @@
 /**
  * Auditable & minimal JS implementation of post-quantum public-key cryptography.
  * Check out individual modules.
+ * @example
+
+import { ml_kem512, ml_kem768, ml_kem1024 } from '@noble/post-quantum/ml-kem';
+import { ml_dsa44, ml_dsa65, ml_dsa87 } from '@noble/post-quantum/ml-dsa';
+import {
+  slh_dsa_sha2_128f, slh_dsa_sha2_128s,
+  slh_dsa_sha2_192f, slh_dsa_sha2_192s,
+  slh_dsa_sha2_256f, slh_dsa_sha2_256s,
+  slh_dsa_shake_128f, slh_dsa_shake_128s,
+  slh_dsa_shake_192f, slh_dsa_shake_192s,
+  slh_dsa_shake_256f, slh_dsa_shake_256s,
+} from '@noble/post-quantum/slh-dsa';
+
  * @module
  */
 throw new Error('root module cannot be imported: import submodules instead. Check out README');
