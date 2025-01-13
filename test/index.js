398bdbcc266518a3623376fb08e2c1d0916aa46a
Paul Miller <paul@paulmillr.com>
2025-01-13 04:54:26 +0000

    Bump micro-should devdep

--- test/index.js
+++ test/index.js
@@ -1,11 +1,3 @@
 import { should } from 'micro-should';
-// import './ml-kem.test.js';
-// import './ml-dsa.test.js';
-import './avcp.test.js'; // Test vectors from 'AVCP'
-
-// ESM is broken.
-import url from 'node:url';
-if (import.meta.url === url.pathToFileURL(process.argv[1]).href) {
-  //should.runParallel(); // 43 seconds
-  should.run(); // 176 seconds
-}
+import './avcp.test.js';
+should.runWhen(import.meta.url);
